a7814be95a0e7ee89f5199328e2b1a27233198c3
Hkwolfx <bourlandalexis@orange.fr>
2024-07-04 12:58:36 +0200

    SeatLayout component added

---
+++ seat-selection/src/components/SeatLayout.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Seat from './Seat';
+import './SeatLayout.css';
+
+interface SeatLayoutProps {
+  layout: string[][];
+  onSeatClick: (row: number, col: number) => void;
+}
+
+const SeatLayout: React.FC<SeatLayoutProps> = ({ layout, onSeatClick }) => {
+  return (
+    <div className="seat-layout">
+      {layout.map((row, rowIndex) => (
+        <div key={rowIndex} className="seat-row">
+          {row.map((seat, colIndex) => (
+            <Seat
+              key={colIndex}
+              status={seat as 'available' | 'selected' | 'unavailable'}
+              onClick={() => onSeatClick(rowIndex, colIndex)}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default SeatLayout;
